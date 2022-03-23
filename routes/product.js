7488edda91ebe0d5ea2af619f07ee382c85a5fb8
0Apexx0 <adityaapexx@gmail.com>
2022-03-24 01:53:01 +0530

    remove add product route and controller

--- routes/product.js
+++ routes/product.js
@@ -7,9 +7,6 @@ const productController = require('../controllers/productController');
 // fetch product page (admin purpose  not with project)
 router.get('/' , productController.addProductPage);
 
-// add product (admin purpose  not with project)
-router.post('/add', productController.addProduct);
-
 // fetch claim offer page
 router.get('/claim', productController.claimProductPage );
 
